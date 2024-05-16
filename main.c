5718c9343f51eb5f16521648c9acf236fd67d3b7
crgimenes <crg@crg.eti.br>
2024-05-16 10:09:49 -0300

    add options

--- main.c
+++ main.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     time_t now = time(NULL);
     struct tm *utc = gmtime(&now);
 
+    int t = 0;
+    char *format = NULL;
+    char buffer[80];
+    int opt;
+
+    while ((opt = getopt(argc, argv, "t:f:")) != -1) {
+        switch (opt) {
+            case 't':
+                t = atoi(optarg);
+                continue;
+            case 'f':
+                format = optarg;
+                continue;
+            default:
+                fprintf(stderr, "Usage: %s [-t timezone] [-f format]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (t < -12 || t > 12) {
+        fprintf(stderr, "Invalid timezone\n");
+        return 1;
+    } 
+
     // Calculate the beat value (Swatch Internet Time)
     int total_seconds_bmt =
-        ((utc->tm_hour + 1) % 24) * 3600 + utc->tm_min * 60 + utc->tm_sec;
+        ((utc->tm_hour + t) % 24) * 3600 + utc->tm_min * 60 + utc->tm_sec;
 
     float beat = total_seconds_bmt /
                  86.4;  // 1 day = 1000 beats so 1 beat = 86.4 seconds
 
-    if (argc == 2) {
-        printf(argv[1], beat);
-        return 0;
+    if (format == NULL) {
+        format = "@%02f\n";
     }
 
-    printf("@%.2f\n", beat);
+    printf(format, beat);
 
     return 0;
 }
